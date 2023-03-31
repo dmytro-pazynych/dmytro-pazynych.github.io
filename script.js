cd68dc4d68d3bcccff2783a550904d1d6499fd2c
Dmytro Pazynych <dpazynych@gmail.com>
2023-03-31 17:28:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function checkAnswer() {
-	var answer = document.querySelector('input[name="age"]:checked').value;
-	if (answer === "27") {
-		document.getElementById("result").innerHTML = "Correct!";
-		window.location.href = "question2.html";
-	} else {
-		document.getElementById("result").innerHTML = "Incorrect. Try again.";
-	}
-}
+// Get the button elements for each question page
+const question1Btn = document.getElementById("question1-btn");
+const question2Btn = document.getElementById("question2-btn");
+const finalBtn = document.getElementById("final-btn");
+
+// Add event listeners to the button elements
+question1Btn.addEventListener("click", () => {
+  location.href = "question1.html";
+});
+
+question2Btn.addEventListener("click", () => {
+  location.href = "question2.html";
+});
+
+finalBtn.addEventListener("click", () => {
+  location.href = "final.html";
+});
